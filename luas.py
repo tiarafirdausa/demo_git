4252983070a71bffc6470f79da4769f8863915bc
Your Nametiarafirdausa <tiarafirdausa94@gmail.com>
2021-09-13 17:45:55 +0700

    initial python

---
+++ luas.py
@@ -0,0 +1,10 @@
+def hitung_luas_lingkaran(r):
+    luas = 3.14 * r ** 2
+    return luas
+
+def hitung_luas_persegi(s):
+    luas = s * s 
+    return luas 
+
+print ("Luas lingkaran dengan jari-jari 7 adalah", hitung_luas_lingkaran(7))
+print ("Luas persegi dengan panjang sisi 4 adalah", hitung_luas_persegi(4))
